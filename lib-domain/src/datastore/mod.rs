03b2127015fa718af21350614ebfb1ae95a9b03e
Shashank Verma <shashank.verma2002@gmail.com>
2025-07-05 20:06:39 +0530

    Move migrations to separate lib
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- lib-domain/src/datastore/mod.rs
+++ lib-domain/src/datastore/mod.rs
@@ -21,7 +21,7 @@ impl Datastore {
     pub(crate) async fn connect() -> Self {
         let url = config::get_db_url();
 
-        Self::migrate(url).await;
+        lib_migrations::migrate_schema(url).await;
 
         let (client, connection) = tokio_postgres::connect(url, NoTls).await.expect("Failed to connect to postgres");
 
@@ -43,13 +43,6 @@ impl Datastore {
             space_stmts,
             user_space_stmts,
         }
-    }
-
-    async fn migrate(url: &str) {
-        let db =
-            sqlx::postgres::PgPoolOptions::new().max_connections(4).connect(url).await.expect("Failed init PgPool");
-
-        sqlx::migrate!("./migrations").run(&db).await.expect("Failed to run migrations");
     }
 }
 
