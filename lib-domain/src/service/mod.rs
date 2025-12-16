fd2a0dab40bf0acd9ff469e698d01d45fbd8c87c
Shashank Verma <shashank.verma2002@gmail.com>
2025-12-16 08:38:13 +0530

    somachron: preview: run job one at a time
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- lib-domain/src/service/mod.rs
+++ lib-domain/src/service/mod.rs
@@ -33,31 +33,24 @@ impl Service<Datastore> {
 
         let total = files.len();
 
-        let tasks =
-            files.into_iter().filter(|f| f.metadata.preview_meta.is_none()).enumerate().map(|(i, file)| async move {
-                let folder = self.ds.get_folder(&file.space_id, &file.parent_node.unwrap()).await?.unwrap();
+        for (i, file) in files.into_iter().filter(|f| f.metadata.preview_meta.is_none()).enumerate() {
+            let file_path = std::path::PathBuf::from(&file.path).join(&file.node_name);
+            println!("processing: {file_path:?}");
 
-                let file_path = std::path::PathBuf::from(&file.path).join(&file.node_name);
-                let space_id = file.space_id.to_string();
+            let folder = self.ds.get_folder(&file.space_id, &file.parent_node.unwrap()).await?.unwrap();
+            let space_id = file.space_id.to_string();
 
-                let file_data = storage
-                    .process_upload_completion(space_id.as_str(), file_path.to_str().unwrap(), file.node_size as usize)
-                    .await?;
+            let file_data = storage
+                .process_upload_completion(space_id.as_str(), file_path.to_str().unwrap(), file.node_size as usize)
+                .await?;
 
-                for data in file_data.into_iter() {
-                    let _ = self
-                        .ds
-                        .upsert_file(&file.user_id.unwrap(), &file.space_id, &folder, file.updated_at, data)
-                        .await?;
-                }
+            for data in file_data.into_iter() {
+                let _ =
+                    self.ds.upsert_file(&file.user_id.unwrap(), &file.space_id, &folder, file.updated_at, data).await?;
+            }
 
-                println!("{i}/{total}");
-
-                Ok::<_, AppError>(())
-            });
-
-        let results = futures::stream::iter(tasks).buffer_unordered(2).collect::<Vec<_>>().await;
-        dbg!(results);
+            println!("{i}/{total}");
+        }
 
         Ok(())
     }
