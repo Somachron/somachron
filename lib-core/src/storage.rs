99fa8899b5c5266335dcfcff553ccbbda4931800
Shashank Verma <shashank.verma2002@gmail.com>
2025-12-16 09:52:14 +0530

    somachron: storage: fix preview data for general type
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- lib-core/src/storage.rs
+++ lib-core/src/storage.rs
@@ -237,8 +237,8 @@ impl Storage {
                     .context("uploading thumbnail for general type")?;
                 remove_file(&thumbnail.path).await?;
 
+                let preview_file_name = format!("preview_{src_file_stem}.jpeg");
                 if media_type == infer::MatcherType::Image {
-                    let preview_file_name = format!("preview_{src_file_stem}.jpeg");
                     let mut r2_preview = r2_path.clone();
                     r2_preview.set_file_name(&preview_file_name);
                     self.r2
@@ -254,7 +254,11 @@ impl Storage {
                         height: thumbnail.height as i32,
                         file_name: thumbnail_file_name,
                     },
-                    preview: media::ImageMeta::default(),
+                    preview: media::ImageMeta {
+                        width: preview.width as i32,
+                        height: preview.height as i32,
+                        file_name: preview_file_name,
+                    },
                     file_name: None,
                 });
             }
