db116c7e6c6a957415a7fe8b2d0e9313f6bfda2e
Shashank Verma <shashank.verma2002@gmail.com>
2026-02-21 12:24:01 +0530

    domain: ds: Move from 500 to 401 when native_app is missing creds
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- lib-domain/src/datastore/native_app.rs
+++ lib-domain/src/datastore/native_app.rs
@@ -13,11 +13,15 @@ pub trait NativeAppDs {
 
 impl NativeAppDs for Datastore {
     async fn validate_native_app(&self, identifier: String) -> AppResult<()> {
-        let _ = self
+        let rows = self
             .db
-            .query_one(&self.native_app_stmts.get_app_by_identifier, &[&identifier])
+            .query(&self.native_app_stmts.get_app_by_identifier, &[&identifier])
             .await
             .map_err(|err| ErrType::DbError.err(err, "Failed to get native app by identifier"))?;
+
+        if rows.is_empty() {
+            return Err(ErrType::Unauthorized.msg("Invalid build"));
+        }
 
         Ok(())
     }
